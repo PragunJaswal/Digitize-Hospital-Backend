5f40c0010ee0511f87f7154bc1416d9bb22cf142
Pragun Jaswal <pragunjaswal2@gmail.com>
2023-04-25 22:06:01 +0530

    sttar

--- main.py
+++ main.py
@@ -21,13 +21,6 @@ origins = [
     "http://localhost:8080",
 ]
 
-app.add_middleware(
-CORSMiddleware,
-allow_origins=["*"], # Allows all origins
-allow_credentials=True,
-allow_methods=["*"], # Allows all methods
-allow_headers=["*"], # Allows all headers
-)
 
 
 my_post= [
@@ -184,6 +177,13 @@ def update(id: int ,post: Post):
 
     return{"data":my_post}
 
+app.add_middleware(
+CORSMiddleware,
+allow_origins=["*"], # Allows all origins
+allow_credentials=True,
+allow_methods=["*"], # Allows all methods
+allow_headers=["*"], # Allows all headers
+)
 
 
 
