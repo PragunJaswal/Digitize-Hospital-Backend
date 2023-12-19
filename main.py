2f535e171187fe30996b49753847ccbefa638966
Pragun Jaswal <pragunjaswal2@gmail.com>
2023-12-19 17:57:53 +0530

    Sdc

--- main.py
+++ main.py
@@ -11,6 +11,28 @@ import psycopg2                     # for databse connection
 from psycopg2.extras import RealDictCursor
 import time
 from datetime import datetime
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+import joblib
+
+
+# Load the dataset
+df = pd.read_csv('main.csv')
+target_variable = 'prognosis'
+
+# Split the data into features (X) and target variable (y)
+X = df.drop(target_variable, axis=1)
+y = df[target_variable]
+
+# Perform one-hot encoding for categorical features
+X = pd.get_dummies(X)
+
+# Encode the target variable
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
+
+
 
 app =FastAPI()
 
@@ -51,6 +73,9 @@ class Post2(BaseModel):
 class Post3(BaseModel):
     id: int
     allocated_time: str
+
+class SymptomsInput(BaseModel):
+    symptoms: list[str]
 
                     #connection with database
 while True:
@@ -217,6 +242,39 @@ def getpost():
     posts = cursor.fetchall()
     return{ "data":posts }
 
+@app.get("/getsymptoms")
+def get_symptoms():
+    symptoms = [
+        'itching', 'skin_rash', 'nodal_skin_eruptions', 'continuous_sneezing', 'shivering', 'chills', 'joint_pain',
+        'stomach_pain', 'acidity', 'ulcers_on_tongue', 'muscle_wasting', 'vomiting', 'burning_micturition',
+        'spotting_ urination', 'fatigue', 'weight_gain', 'anxiety', 'cold_hands_and_feets', 'mood_swings',
+        'weight_loss', 'restlessness', 'lethargy', 'patches_in_throat', 'irregular_sugar_level', 'cough', 'high_fever',
+        'sunken_eyes', 'breathlessness', 'sweating', 'dehydration', 'indigestion', 'headache', 'yellowish_skin',
+        'dark_urine', 'nausea', 'loss_of_appetite', 'pain_behind_the_eyes', 'back_pain', 'constipation',
+        'abdominal_pain', 'diarrhoea', 'mild_fever', 'yellow_urine', 'yellowing_of_eyes', 'acute_liver_failure',
+        'fluid_overload', 'swelling_of_stomach', 'swelled_lymph_nodes', 'malaise', 'blurred_and_distorted_vision',
+        'phlegm', 'throat_irritation', 'redness_of_eyes', 'sinus_pressure', 'runny_nose', 'congestion', 'chest_pain',
+        'weakness_in_limbs', 'fast_heart_rate', 'pain_during_bowel_movements', 'pain_in_anal_region', 'bloody_stool',
+        'irritation_in_anus', 'neck_pain', 'dizziness', 'cramps', 'bruising', 'obesity', 'swollen_legs',
+        'swollen_blood_vessels', 'puffy_face_and_eyes', 'enlarged_thyroid', 'brittle_nails', 'swollen_extremities',
+        'excessive_hunger', 'extra_marital_contacts', 'drying_and_tingling_lips', 'slurred_speech', 'knee_pain',
+        'hip_joint_pain', 'muscle_weakness', 'stiff_neck', 'swelling_joints', 'movement_stiffness',
+        'spinning_movements', 'loss_of_balance', 'unsteadiness', 'weakness_of_one_body_side', 'loss_of_smell',
+        'bladder_discomfort', 'foul_smell_of urine', 'continuous_feel_of_urine', 'passage_of_gases',
+        'internal_itching', 'toxic_look_(typhos)', 'depression', 'irritability', 'muscle_pain', 'altered_sensorium',
+        'red_spots_over_body', 'belly_pain', 'abnormal_menstruation', 'dischromic _patches', 'watering_from_eyes',
+        'increased_appetite', 'polyuria', 'family_history', 'mucoid_sputum', 'rusty_sputum', 'lack_of_concentration',
+        'visual_disturbances', 'receiving_blood_transfusion', 'receiving_unsterile_injections', 'coma',
+        'stomach_bleeding', 'distention_of_abdomen', 'history_of_alcohol_consumption', 'fluid_overload', 'blood_in_sputum',
+        'prominent_veins_on_calf', 'palpitations', 'painful_walking', 'pus_filled_pimples', 'blackheads', 'scurrying',
+        'skin_peeling', 'silver_like_dusting', 'small_dents_in_nails', 'inflammatory_nails', 'blister',
+        'red_sore_around_nose', 'yellow_crust_ooze'
+    ]
+
+    return [{"symptom": symptom} for symptom in symptoms]
+
+
+
 @app.get("/get/status/doctor/{registration_id}")
 def getpost(registration_id: int):
     cursor.execute(f"""SELECT * FROM doctors WHERE registration_id = {registration_id}""")
@@ -280,6 +338,37 @@ def post(payload: Post2):
     # conn.rollback()
     # print(new)
     return{"Success":new }
+
+
+@app.post("/predict/deptt")
+# Now, let's make predictions for a user with symptoms using the saved model
+def model (symptoms_input: SymptomsInput):
+    loaded_model = joblib.load('main.pkl')
+
+    user_symptoms = ['itching','skin_rash','nodal_skin_eruptions']
+    user_data = pd.DataFrame(index=[0], columns=X.columns)
+    user_data[:] = 0
+    user_data.loc[:, symptoms_input.symptoms] = 1
+
+    # Predict the disease for the user using the loaded model
+    predictions_nb = loaded_model.predict(user_data)
+    probability_nb = loaded_model.predict_proba(user_data)[:, 1]
+    predicted_disease_nb = label_encoder.inverse_transform(predictions_nb)
+
+    # print(f"Predicted Disease (Naive Bayes): {predicted_disease_nb[0]}")
+    formatted_probability = float(probability_nb * 1000)  
+    print(f"Probability Estimate (Naive Bayes): {formatted_probability}")
+
+    if formatted_probability <= 200:
+        Predicted_deptt = "GENERAL"
+    else:
+        Predicted_deptt = predicted_disease_nb[0]
+
+    return{"Department":Predicted_deptt}
+    
+
+
+
 
 #                                           get a item with id
 
